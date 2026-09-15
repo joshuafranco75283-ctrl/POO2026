29343aa0098e39e424093a3a08f16ccd39c6380b
joshuafranco75283-ctrl <joshua.franco75283@ucaldas.edu.co>
2026-09-15 10:51:29 -0500

    Mejora ejercicio2

--- Talleres/Taller1/Ejercicio2/TestProduct.java
+++ Talleres/Taller1/Ejercicio2/TestProduct.java
@@ -2,30 +2,36 @@
 public class TestProduct{
     public static void main(String[] args) {
         
-        String[] newName = {"", "Laptop", "Smartphone"};
-        double[] newPrice = {500.0, -3.0, 800.0};
-        int[] newStock = {10, 5, -4};
+        String[] newName = {"", "Laptop", "Smartphone", "Tablet"};
+        double[] newPrice = {500.0, -3.0, 800.0, 300.0};
+        int[] newStock = {10, 5, -4, 7};
         for (int i = 0; i < newName.length; i++) {
             System.out.println("Cambiando el producto " + (i + 1) + ":");
             Product product = new Product();
             boolean acceptedName = product.setName(newName[i]);
             if (acceptedName){
                 System.out.println("Product Name: " + product.getName());
-            }else{
-                System.out.println("Error: Nombre inválido" + " Se conseva el nombre " + product.getName());}
-             
-            boolean acceptedPrice = product.setPrice(newPrice[i]);
-            if(acceptedPrice){
+                boolean acceptedPrice = product.setPrice(newPrice[i]);
+                if(acceptedPrice){
                 System.out.println("Product Price: " + product.getPrice());
-            } else {
-                System.out.println("Error: Precio inválido" + " Se conserva el precio " + product.getPrice() + "$");
-            }
-            boolean acceptedStock = product.setStock(newStock[i]);
-            if(acceptedStock){
-                System.out.println("Product Stock: " + product.getStock());
-            } else {
-                System.out.println("Error: Stock inválido" + " Se conserva el stock " + product.getStock());
-            }
+                boolean acceptedStock = product.setStock(newStock[i]);
+                    if(acceptedStock){
+                    System.out.println("Product Stock: " + product.getStock());
+                    System.out.println("Estado aceptado valor total del producto: " + (product.getPrice() * product.getStock()));
+                } else {
+                    System.out.println("Error: Stock inválido " + " Estado rechazado");
+                }
+                } else {
+                System.out.println("Error: Precio inválido " + " Estado rechazado");
+
+
+            }}else{
+                System.out.println("Error: Nombre inválido " + "Estado rechazado ");}
+             
+            
+            
+            
+           
             
         }
     }
